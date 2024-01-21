d64ee6423078262dc1cbddfe0fd8bd3638d437b6
Parth Singh <parth3743@gmail.com>
2024-01-21 18:06:39 +0530

    Frontend Completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,58 @@
+import { useState, useEffect } from "react";
+import Todo from "./components/Todo";
+import { addTodo, getAllTodo, updateTodo, deleteTodo } from "./utils/HandleApi";
+
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [text, setText] = useState("");
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [todoId, setTodoId] = useState("");
+
+  useEffect(() => {
+    getAllTodo(setTodo);
+  }, []);
+
+  const updateMode = (_id, text) => {
+    setIsUpdating(true);
+    setText(text);
+    setTodoId(_id);
+  };
+
   return (
     <div className="App">
-      <h1>Todo App</h1>
+      <div className="container">
+        <h1>TODO APP</h1>
+        <div className="top">
+          <input
+            type="text"
+            placeholder="Add Todo..."
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <div
+            className="add"
+            onClick={
+              isUpdating
+                ? () => updateTodo(todoId, text, setText, setTodo, setIsUpdating)
+                : () => addTodo(text, setText, setTodo)
+            }
+          >
+            {isUpdating ? "Update" : "Add"}
+          </div>
+        </div>
+
+        <div className="list">
+          {todo.data &&
+            todo.data.map((item) => (
+              <Todo
+                key={item._id}
+                text={item.text}
+                updateMode={() => updateMode(item._id, item.text)}
+                deleteTodo={() => deleteTodo(item._id, setTodo)}
+              />
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/utils/HandleApi.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+
+const baseURL = "http://localhost:8000/todo";
+
+const getAllTodo = async (setTodo) => {
+  await axios
+    .get(`${baseURL}/get-todo`)
+    .then(({ data }) => {
+      console.log("data ---> ", data);
+      setTodo(data);
+    })
+    .catch((err) => console.log(err.message));
+};
+
+const addTodo = async (text, setText, setTodo) => {
+  await axios.post(`${baseURL}/add-todo`, { text }).then((data) => {
+    console.log(data);
+    setText("");
+    getAllTodo(setTodo);
+  });
+};
+
+const updateTodo = async (todoId, text, setText, setTodo, setIsUpdating) => {
+  await axios
+    .post(`${baseURL}/update-todo`, { _id: todoId, text })
+    .then((data) => {
+      setText("");
+      setIsUpdating(false);
+      getAllTodo(setTodo);
+    })
+    .catch((err) => console.log(err));
+};
+
+const deleteTodo = async (_id, setTodo) => {
+  await axios
+    .post(`${baseURL}/delete-todo`, { _id })
+    .then((data) => {
+      getAllTodo(setTodo);
+    })
+    .catch((err) => console.log(err));
+};
+
+export { getAllTodo, addTodo, updateTodo, deleteTodo };
